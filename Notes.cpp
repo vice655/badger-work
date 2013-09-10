20ba1f729d76a80037df1511f5a0aa1f91a6222a
vice655 <vice655@users.noreply.github.com>
2013-09-10 23:14:38 +0100

    Initial CREATRUGNREGKNREIPGR

---
+++ Notes.cpp
@@ -0,0 +1,12 @@
+#Silent's TODO list, its like badger's - except I make mine through GitHub;
+
+#cs_compound, cs_italy, de_cbble, de_dust, de_inferno, de_port, de_tides and de_train
+#  MUST ALLOCATE WEAPON SPAWN
+#not only THAT BUT I GOTTA DO
+#ttt_magma, ttt_metropolis, ttt_tundra, ttt_skyscraper, ttt_cloverfield_b4, ttt_crummycradle_a4,
+#ttt_fallout, ttt_horriblenight_v1, ttt_minecraft_b5, ttt_rooftops_a2, ttt_starfish_island_v1_2, 
+#ttt_roy_the_ship and de_dolls
+#ITS CALLED BULLYING, DEMOTE THAT SON OF A BITCH.
+
+#DUE TO BE COMPLETED FRIDAY 13TH OF SEPTEMBER.
+#YES, ITS FRIDAY THE THIRTEENTH - GOT A PROBLEM WITH THAT?
